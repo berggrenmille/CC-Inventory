5bab8f89171b77ab8c35ec056bc6ae876ed827e8
berggrenmille <berggrenmille@hotmail.se>
2024-09-09 18:22:20 +0200

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -11,6 +11,31 @@ local function debugPrint(...)
     end
 end
 
+local function tprint(tbl, indent)
+    if not indent then indent = 0 end
+    local toprint = string.rep(" ", indent) .. "{\r\n"
+    indent = indent + 2
+    for k, v in pairs(tbl) do
+        toprint = toprint .. string.rep(" ", indent)
+        if (type(k) == "number") then
+            toprint = toprint .. "[" .. k .. "] = "
+        elseif (type(k) == "string") then
+            toprint = toprint .. k .. "= "
+        end
+        if (type(v) == "number") then
+            toprint = toprint .. v .. ",\r\n"
+        elseif (type(v) == "string") then
+            toprint = toprint .. "\"" .. v .. "\",\r\n"
+        elseif (type(v) == "table") then
+            toprint = toprint .. tprint(v, indent + 2) .. ",\r\n"
+        else
+            toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
+        end
+    end
+    toprint = toprint .. string.rep(" ", indent - 2) .. "}"
+    return toprint
+end
+
 local function runGui()
     local main = basalt.createFrame() -- The main frame/most important frame in your project
     local list = main
@@ -99,6 +124,7 @@ local function runGui()
     removeButton:onClick(removeClicked)
 
     list:onSelect(function(self, event, item)
+        debugPrint("Selected item: " .. tprint(item))
         inputName:setValue(item.args.name)
         inputAmount:setValue(item.args.count)
         isFluid:setValue(item.args.isFluid)
